a791192d93ff5871e25fbb165aa5ddd6cc63d410
Gino Hong <ginohong7@gmail.com>
2022-05-18 04:49:29 +0000

    Completed issue-3-user-can-start-their-car.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var $f1 = document.querySelector('.f1');
 
 document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
   if (e.key === 'ArrowUp') {
     $f1.className = 'f1 north';
   } else if (e.key === 'ArrowDown') {
@@ -9,5 +10,16 @@ document.addEventListener('keydown', function (e) {
     $f1.className = 'f1 east';
   } else if (e.key === 'ArrowLeft') {
     $f1.className = 'f1 west';
+  } else if (e.key === ' ') {
+    setInterval(start, 16);
   }
 });
+
+var x = 0;
+
+function start() {
+  if ($f1.className === 'f1 east') {
+    x = x + 7;
+    $f1.style.left = x + 'px';
+  }
+}
